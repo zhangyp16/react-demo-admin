0ae74ac6739ac7c84a4a9fc0774afb34a6396637
zhangyongping <zhangyongping@tezign.com>
2019-09-02 16:28:39 +0800

    页面

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-demo-admin
+
+## react + webpack + babel
+
+## get started
+
+- cd react-demo-adimn
+- npm install
+- npm start
---
+++ src/component/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class App extends React.Component {
+    render(){
+        <div id=''></div>
+    }
+}
+
+export default App
