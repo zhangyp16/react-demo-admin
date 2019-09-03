cf140123bbad52857e57854ad8ddd5f576ce689f
zhangyongping <zhangyongping@tezign.com>
2019-09-03 10:47:11 +0800

    左侧导航

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Lefter from './Lefter'
+import '../index.css'
 
 class App extends React.Component {
     render(){
-        <div id=''></div>
+        return (
+            <div className='leftMenu'>
+                <Lefter />
+            </div>
+            
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './component/App'
+import App from './component/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
